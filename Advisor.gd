958ecec45cb26af9683155c471323af94650be25
Andrew Strauch <rememberx@gmail.com>
2016-12-10 18:38:01 -0500

    Advisor signaling is working on mouse over and hotkey

--- Advisor.gd
+++ Advisor.gd
@@ -1,13 +1,21 @@
-extends Node
+extends Node2D
 
-const ADVISOR_ALIGNMENT = {
-    SUPER_LEFT = 1,
-    LEFT = 2,
-    MODERATE = 3,
-    RIGHT = 4,
-    SUPER_RIGHT = 5
-}
+var hotkey
+
+#Talking points
 
 func _ready():
-	pass
+	get_node("Detect").connect("mouse_enter", self, "on_detect_body_enter")
+	#Refactor this to be outside class
+	hotkey = Input.is_key_pressed(KEY_1)
+	set_fixed_process(true)
 
+func _fixed_process(delta):
+	if (hotkey):
+		print("Hotkey!")
+
+func on_detect_body_enter():
+	print("Wolf!")
+	
+func set_hotkey(myHotkey):
+	hotkey = myHotkey
