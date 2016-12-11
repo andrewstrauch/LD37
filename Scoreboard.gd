76c13701e28c1f2e56f057b2702289f8c512e9df
James Pizzurro <james.pizzurro@it.com>
2016-12-11 13:18:57 -0500

    Added game state logic and scoreboard

---
+++ Scoreboard.gd
@@ -0,0 +1,43 @@
+extends Node
+
+var _gamestate
+
+var _truthiness_bar
+var _left_appeal_bar
+var _right_appeal_bar
+
+var _truthiness_percentage
+var _left_appeal_percentage
+var _right_appeal_percentage
+
+func _on_appeal_burndown_timer_timeout():
+	_gamestate.burndown_appeal()
+
+func _ready():
+	_gamestate = get_node("/root/GameState")
+
+	_truthiness_bar = get_node("TruthinessBar")
+	_truthiness_bar.set_max(_gamestate.MAX_TRUTHINESS)
+	_left_appeal_bar = get_node("LeftAppealBar")
+	_left_appeal_bar.set_max(_gamestate.MAX_LEFT_APPEAL)
+	_right_appeal_bar = get_node("RightAppealBar")
+	_right_appeal_bar.set_max(_gamestate.MAX_RIGHT_APPEAL)
+
+	_truthiness_percentage = _truthiness_bar.get_node("TruthinessPercentage")
+	_left_appeal_percentage = _left_appeal_bar.get_node("LeftAppealPercentage")
+	_right_appeal_percentage = _right_appeal_bar.get_node("RightAppealPercentage")
+
+	var _appeal_burndown_timer = get_node("AppealBurndownTimer")
+	_appeal_burndown_timer.connect("timeout", self, "_on_appeal_burndown_timer_timeout")
+
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	# update all components using gamestate
+	_truthiness_bar.set_value(_gamestate.current_truthiness)
+	_truthiness_percentage.set_text(str(_gamestate.current_truthiness) + "%")
+	_left_appeal_bar.set_value(_gamestate.current_left_appeal)
+	_left_appeal_percentage.set_text(str(_gamestate.current_left_appeal) + "%")
+	_right_appeal_bar.set_value(_gamestate.current_right_appeal)
+	_right_appeal_percentage.set_text(str(_gamestate.current_right_appeal) + "%")
+	pass
