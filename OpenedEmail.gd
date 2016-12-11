73639a98bb9339d37b8aebe5bd3032e053535243
Andrew Strauch <rememberx@gmail.com>
2016-12-11 12:11:33 -0500

    Opened email approve and reject functionality working

---
+++ OpenedEmail.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var headlineName
+#Vars for headline values here?
+
+var c
+
+func _ready():
+	#set_color("Panel/ApproveButton", Color(1,0,0))
+	#get_node("Panel/ApproveButton").
+	get_node("Panel/ApproveButton").set_text("Approve")
+	get_node("Panel/RejectButton").set_text("Reject")
+	get_node("Panel/ApproveButton").connect("pressed", self, "on_approve_button_pressed")
+	get_node("Panel/RejectButton").connect("pressed", self, "on_reject_button_pressed")
+	c = get_node("/root/Controller")
+	get_node("Panel/BurndownTimer").connect("timeout", self, "on_burndown_timer_timeout")
+
+func pass_timer_time(time):
+		get_node("Panel/BurndownTimer").set_wait_time(time)
+		get_node("Panel/BurndownTimer").start()
+	
+func on_approve_button_pressed():
+	print("Approved!")
+	handle_email_close("approve", "Test")
+	
+func on_reject_button_pressed():
+	print("Rejected!")
+	handle_email_close("reject", "Test")
+	
+func on_burndown_timer_timeout():
+	handle_email_close(null, "Test")
+
+func handle_email_close(button, headline):
+	c.readingAnEmail = false
+	#Set other values in controller for handling effects
+	queue_free()
--- controller.gd
+++ controller.gd
@@ -7,9 +7,3 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	pass
-
-func get_reading_an_email():
-	return readingAnEmail
-
-func set_reading_an_email(myBool):
-	readingAnEmail = myBool
