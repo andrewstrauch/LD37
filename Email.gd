caa243c5431724daae6ada377a3cfc00a0d250fb
James Pizzurro <james.pizzurro@it.com>
2016-12-11 00:11:29 -0500

    Email now fetches random headline; randomized headline list

--- Email.gd
+++ Email.gd
@@ -2,6 +2,8 @@ extends Node
 
 var _burndown_timer
 var _burndown_progress_bar
+var _headlines
+var _headline
 
 func get_burndown_timer_time_left():
 	return _burndown_timer.get_time_left()
@@ -14,6 +16,13 @@ func _ready():
 	_burndown_timer.connect("timeout", self, "_on_burndown_timer_timeout")
 
 	_burndown_progress_bar = get_node("EmailSprite").get_node("BurndownProgressBar")
+	
+	_headlines = get_node("/root/Headlines")
+	_headline = _headlines.get_random_headline()
+	var _headline_text_to_display = _headline.text
+	if _headline_text_to_display.length() > 60:
+		_headline_text_to_display = _headline_text_to_display.substr(0, 60) + "..."
+	get_node("EmailSprite").get_node("HeadlineText").set_bbcode(_headline_text_to_display)
 
 	set_fixed_process(true)
 	pass
