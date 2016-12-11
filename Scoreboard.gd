1147402155a24a76ba16201f382173c598713d74
James Pizzurro <james.pizzurro@it.com>
2016-12-11 13:48:35 -0500

    Scoreboard display cleanup

--- Scoreboard.gd
+++ Scoreboard.gd
@@ -28,16 +28,17 @@ func _ready():
 	_right_appeal_percentage = _right_appeal_bar.get_node("RightAppealPercentage")
 
 	var _appeal_burndown_timer = get_node("AppealBurndownTimer")
+	_appeal_burndown_timer.set_wait_time(_gamestate.APPEAL_BURNDOWN_TIMER_INTERVAL)
 	_appeal_burndown_timer.connect("timeout", self, "_on_appeal_burndown_timer_timeout")
+	_appeal_burndown_timer.start()
 
 	set_fixed_process(true)
 
 func _fixed_process(delta):
 	# update all components using gamestate
 	_truthiness_bar.set_value(_gamestate.current_truthiness)
-	_truthiness_percentage.set_text(str(_gamestate.current_truthiness) + "%")
+	_truthiness_percentage.set_text(str(int((_gamestate.current_truthiness / float(_gamestate.MAX_TRUTHINESS)) * 100)) + "%")
 	_left_appeal_bar.set_value(_gamestate.current_left_appeal)
-	_left_appeal_percentage.set_text(str(_gamestate.current_left_appeal) + "%")
+	_left_appeal_percentage.set_text(str(int((_gamestate.current_left_appeal / float(_gamestate.MAX_LEFT_APPEAL)) * 100)) + "%")
 	_right_appeal_bar.set_value(_gamestate.current_right_appeal)
-	_right_appeal_percentage.set_text(str(_gamestate.current_right_appeal) + "%")
-	pass
+	_right_appeal_percentage.set_text(str(int((_gamestate.current_right_appeal / float(_gamestate.MAX_RIGHT_APPEAL)) * 100)) + "%")
